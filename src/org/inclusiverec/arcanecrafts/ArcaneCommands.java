293e14e0765b88c6b56c4485d65735f7251e6d94
usevalue <jurassic_pollard@hotmail.com>
2015-05-04 08:47:05 -0400

    Created classes for config loading and command handling.  No implementation at this stage.

---
+++ src/org/inclusiverec/arcanecrafts/ArcaneCommands.java
@@ -0,0 +1,14 @@
+package org.inclusiverec.arcanecrafts;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ArcaneCommands implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+}
---
+++ src/org/inclusiverec/arcanecrafts/ArcaneConfig.java
@@ -0,0 +1,13 @@
+package org.inclusiverec.arcanecrafts;
+
+import org.bukkit.plugin.Plugin;
+
+public class ArcaneConfig {
+
+    public ArcaneConfig(Plugin plugin) {
+
+    }
+    public boolean getBoolean() {
+        return false;
+    }
+}
